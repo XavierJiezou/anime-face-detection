ad62af9aef13ab3fa2f1f6bd8659409700569c5d
XavierJiezou <878972272@qq.com>
2021-04-22 18:02:59 +0800

    first commit

---
+++ example/hog_anime_face_detect.py
@@ -0,0 +1,26 @@
+'''Module
+pip install opencv-python
+pip install dlib
+'''
+import cv2
+import dlib
+import sys
+
+
+def hog_anime_face_detect(file_name):
+    img = cv2.imread(file_name) # 读取图片
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 图片灰度化
+    img_gray = cv2.equalizeHist(img_gray)  # 直方图均衡化
+    face_detector = dlib.simple_object_detector('../model/hog_anime_face_detect.svm') # 加载检测器
+    faces = face_detector(img_gray)
+    for face in faces: # 遍历所有检测到的动漫脸
+        left = face.left()
+        top = face.top()
+        right = face.right()
+        bottom = face.bottom()
+        cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 255), 5) # 绘制矩形框
+    cv2.imwrite(f'../result/hog_anime_face_detect_{file_name[-5]}.jpg', img)  # 保存检测结果
+
+
+if __name__ == '__main__':
+    hog_anime_face_detect(sys.argv[1])
---
+++ example/mlp_anime_face_detect.py
@@ -0,0 +1,27 @@
+'''Modules
+pip install animeface
+pip install opencv-python
+'''
+import cv2
+import animeface
+from PIL import Image
+import sys
+
+
+def mlp_anime_face_detect(file_name):
+    img = cv2.imread(file_name)  # 读取图片
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 图片灰度化
+    img_gray = cv2.equalizeHist(img_gray)  # 直方图均衡化
+    faces = animeface.detect(Image.fromarray(img_gray))  # 人脸检测
+    for each in faces:  # 遍历所有检测到的动漫脸
+        temp = each.face.pos
+        x = temp.x
+        y = temp.y
+        w = temp.width
+        h = temp.height
+        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 5)  # 绘制矩形框
+    cv2.imwrite(f'../result/mlp_anime_face_detect_{file_name[-5]}.jpg', img)  # 保存检测结果
+
+
+if __name__ == '__main__':
+    mlp_anime_face_detect(sys.argv[1])
